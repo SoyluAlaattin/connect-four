1d7c49a1d8381454598d0aeaecf74ed066deb875
soylu3450 <asoylu677@gmail.com>
2024-01-23 18:50:04 +0300

    game ekranı ızgaranın sayfa üzerindeki yeri

--- src/screen/game.js
+++ src/screen/game.js
@@ -242,7 +242,7 @@ const Game = () => {
     },
     title: {
       fontSize: "100px",
-      marginBottom: "40px",
+      marginBottom: "10px",
     },
     gridContainer: {
       display: "flex",
@@ -255,7 +255,7 @@ const Game = () => {
       boxShadow: "0px 0px 5000px rgba(0, 0, 0, 99.5)",
     },
     grid: {
-      padding: "20px",
+      padding: "10px",
       position: "relative",
     },
     sideCell: {
